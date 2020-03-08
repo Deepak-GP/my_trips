78613f9b960f2b2181125061fe69d53833eb9b5a
G P <deepak.gp@siemens.com>
2020-03-08 21:36:16 +0530

    Removed debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
         builder: (context, state) {
           if(state is Uninitialized)
